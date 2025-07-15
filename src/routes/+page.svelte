a1bcf2e66024705339978d2966102626debcf42a
Sammy <sammysamkough@hotmail.com>
2025-07-15 17:48:21 -0400

    if on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,14 @@
+<script>
+	let { data } = $props();
+	let { session } = $derived(data);
+</script>
+
 <h1>snarfs</h1>
+
+<h2>
+	{#if session}
+		<a href="/dashboard">go to dashboard</a>
+	{:else}
+		<a href="/login">login</a>
+	{/if}
+</h2>
