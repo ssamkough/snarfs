6511ad21803c8e4dccfdf0a86e8aa3f4fc55b935
Sammy <sammysamkough@hotmail.com>
2025-10-12 23:56:48 -0400

    netlify adapter

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -11,8 +11,17 @@
 </script>
 
 <header>
-	<img src="logo.svg" alt="snarfs logo" width="60" />
-	<button onclick={logout}>Logout</button>
+	<div style="display: flex; gap: 1em;">
+		<img src="logo.svg" alt="snarfs logo" width="60" />
+		<button onclick={logout}>Logout</button>
+	</div>
+	<div>
+		<input id="color-mode" type="checkbox" name="color-mode" />
+		<label for="color-mode">
+			<span class="dark-mode-hide">Dark Mode</span>
+			<span class="light-mode-hide">Light Mode</span>
+		</label>
+	</div>
 </header>
 <main>
 	{@render children()}
